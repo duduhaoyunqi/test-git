fa4b4088ca319cb75c6fc3778f956091e9ae71ee
“duduhaoyunqi” <diaoke901214@gmail.com>
2022-11-11 14:28:27 +0800

    fixed typo

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "prod" {
   engine_version       = "5.6.17"
   instance_class       = "db.t2.micro"
   name                 = "mydb"
-  username             = "foo"
-  password             = "bar"
+  username             = "diaoke"
+  password             = "123456"
   db_subnet_group_name = "my_database_subnet_group"
   parameter_group_name = "default.mysql5.6"
 }
